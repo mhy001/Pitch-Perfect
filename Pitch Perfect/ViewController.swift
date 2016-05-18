97f716c73002750d01438b31ea819339b050ed3d
mhy001 <mark.h.yang@gmail.com>
2016-05-17 21:48:08 -0700

    Autolayout and Buttons

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(sender: AnyObject) {
+        print("record button pressed")
+        recordingLabel.text = "Recording in progress"
+    }
 
+    @IBAction func stopRecording(sender: AnyObject) {
+        print("stop recording button pressed")
+    }
 }
 
